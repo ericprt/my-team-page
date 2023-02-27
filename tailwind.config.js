87c63642701c1014628547afb6e3c5dcedf3b049
Eric Pratama <ericpratama50@gmail.com>
2023-02-28 06:19:01 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+    },
+    fontFamily: {
+      poppins: "'Poppins', sans-serif",
+      ptSerif: "'PT Serif', serif",
+      montserrat: "'Montserrat', sans-serif",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
